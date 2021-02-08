38ad8501c352cad8b0d07fc866e6687636109104
Jose Riera <rieraja@bc.edu>
2021-02-08 15:39:42 -0500

    cycle through photos

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("👍 viewDidLoad has run")
@@ -18,9 +20,30 @@ class ViewController: UIViewController {
 
     }
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎 The message button was pressed!")
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        print(imageNumber)
+        imageView.image = UIImage(named: "image" + String(imageNumber))
+        //or "image/(imageNumber)"
+        imageNumber = imageNumber+1
+        if imageNumber == 10{
+            imageNumber = 0
+        }
+        
+        
+        
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bombMessage = "You Are Da Bomb!"
+//
+//        if messageLabel.text == awesomeMessage{
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        }else if messageLabel.text == greatMessage{
+//            messageLabel.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//        }else{
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//        }
     }
     
 
